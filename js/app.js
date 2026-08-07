741854e58423bb2be67fbecc842bcb857c796999
Tachy <cordmin@gmail.com>
2026-08-07 14:37:34 +0900

    Set vertical margin offset (fixedH) to 270px as requested

--- js/app.js
+++ js/app.js
@@ -1455,7 +1455,7 @@ function showHelp() { document.getElementById('help-modal').style.display = 'fle
       const isTeacherMode = document.getElementById('board-area').classList.contains('teacher-mode');
       
       const fixedW = 60; 
-      const fixedH = isTeacherMode ? 300 : 250; 
+      const fixedH = isTeacherMode ? 320 : 270; 
       
       let scalableAvailW = workspace.clientWidth - fixedW;
       let scalableAvailH = workspace.clientHeight - fixedH;
